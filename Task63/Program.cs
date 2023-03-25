cac2093130b5a338df939ced44ae52080074c354
Aoi <naztya2001@bk.ru>
2023-03-25 10:50:59 +0300

    task65

---
+++ Task63/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Input number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+NumberLine(number);
+
+void NumberLine(int num)
+{
+    if(num == 0) return;
+    NumberLine(num-1);
+    Console.Write($"{num} ");
+}
---
+++ Task65/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+Console.WriteLine("Input M");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if(m>n) NumberInLineBigToSmall(m,n);
+else NumberInLineSmallToBig(m,n);
+
+void NumberInLineBigToSmall(int number1, int number2)
+{
+    if(number2 == number1) 
+    {
+        Console.Write(number1);
+        return;
+    }
+    Console.Write($"{number1} ");
+    NumberInLineBigToSmall(number1-1, number2);
+}
+
+void NumberInLineSmallToBig(int number1, int number2)
+{
+    if(number1 == number2) 
+    {
+        Console.Write(number1);
+        return;
+    }
+    Console.Write($"{number1} ");
+    NumberInLineSmallToBig(number1+1, number2);
+}
