e46c9b14bdba73d9bee5130e8ec03b2fc83df43d
Aoi <naztya2001@bk.ru>
2023-03-25 18:37:05 +0300

    task64

--- Task64/Program.cs
+++ Task64/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void NumberLine(int num)
+{
+    Console.Write($"{num}, ");
+    if(num == 0) return;
+    NumberLine(num-1);
+}
+
+Console.WriteLine("Input N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+NumberLine(n);
