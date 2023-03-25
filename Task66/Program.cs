e2b738c0c793a416196fec4c1b2bdde09510401a
Aoi <naztya2001@bk.ru>
2023-03-25 19:08:09 +0300

    task66

---
+++ Task66/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Input M");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (m > n) 
+{
+    Console.WriteLine(NumberInLineBigToSmall(m, n));
+}
+else 
+{
+    Console.WriteLine(NumberInLineSmallToBig(m, n));
+}
+
+int NumberInLineBigToSmall(int number1, int number2)
+{
+    if (number2 == number1) return number1;
+    int sum = NumberInLineBigToSmall(number1 - 1, number2);
+    sum += number1; 
+    return sum;
+}
+
+int NumberInLineSmallToBig(int number1, int number2)
+{
+    if (number2 == number1) return number1; 
+    int sum = NumberInLineSmallToBig(number1 + 1, number2);
+    sum += number1; 
+    return sum;
+}
+
