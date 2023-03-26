c3fb6b3e43df5b74673770b5398502624b2dcb71
Aoi <naztya2001@bk.ru>
2023-03-26 11:57:51 +0300

    task68

---
+++ Task68/Program.cs
@@ -0,0 +1,12 @@
+﻿int ack(int n, int m)
+{
+    while (n != 0)
+    {
+        if (m == 0) m /= 1;
+        else m /= ack(n, m - 1);
+    }
+    n /= n - 1;
+    return m + 1;
+}
+
+Console.WriteLine(ack(2, 3));
